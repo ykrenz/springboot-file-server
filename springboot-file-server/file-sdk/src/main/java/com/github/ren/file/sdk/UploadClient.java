a16260b66ba4c7706d8dba5f111ea6023bb2a339
renxiaokui <804619098@qq.com>
2021-04-30 19:10:45 +0800

    文件上传客户端

--- springboot-file-server/file-sdk/src/main/java/com/github/ren/file/sdk/UploadClient.java
+++ springboot-file-server/file-sdk/src/main/java/com/github/ren/file/sdk/UploadClient.java
@@ -2,13 +2,14 @@ package com.github.ren.file.sdk;
 
 import java.io.File;
 import java.io.InputStream;
+import java.io.Serializable;
 
 /**
- * @author Mr Ren
- * @Description: 上传文件客户端
- * @date 2021/3/31 14:41
+ * @Description 上传文件客户端
+ * @Author ren
+ * @Since 1.0
  */
-public interface UploadClient {
+public interface UploadClient extends Serializable {
 
     /**
      * <yourObjectName>表示上传文件时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
@@ -16,7 +17,8 @@ public interface UploadClient {
 
     /**
      * 本地文件上传
-     * @param file 文件
+     *
+     * @param file           文件
      * @param yourObjectName
      * @return
      */
@@ -24,7 +26,8 @@ public interface UploadClient {
 
     /**
      * 上传文件流
-     * @param is 文件流
+     *
+     * @param is             文件流
      * @param yourObjectName
      * @return
      */
@@ -32,7 +35,8 @@ public interface UploadClient {
 
     /**
      * 上传字节数组
-     * @param content 字节数组
+     *
+     * @param content        字节数组
      * @param yourObjectName
      * @return
      */
@@ -40,11 +44,11 @@ public interface UploadClient {
 
     /**
      * 上传网络流
-     * @param url 文件地址
+     *
+     * @param url            文件地址
      * @param yourObjectName
      * @return
      */
     String upload(String url, String yourObjectName);
-
 
 }
