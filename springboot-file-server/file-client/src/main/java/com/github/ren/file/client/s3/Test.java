597c4a5f1c4bde312eb71310ec110d9f9ff1739a
renxiaokui <804619098@qq.com>
2021-07-07 19:11:59 +0800

    文件上传客户端

--- springboot-file-server/file-client/src/main/java/com/github/ren/file/client/s3/Test.java
+++ springboot-file-server/file-client/src/main/java/com/github/ren/file/client/s3/Test.java
@@ -9,16 +9,17 @@ public class Test {
     static String objectPath = "E:\\Xftp-7.0.0071p.exe";
 
     public static void main(String[] args) {
-//        AWSCredentials credentials = new BasicAWSCredentials("LTAI5t8fsEn8Ni6YN1nLZGbr","pBq8nswLkSbBI7gQvfqi7y99BHYlMo");
+//        AWSCredentials credentials = new BasicAWSCredentials("LTAI5tHxmHNVkR5RHmD5xjyU","35iSNN2FJCJyRVw7nRfCGYJKJ7Lzga");
 //
 //        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
 //                .withCredentials(new AWSStaticCredentialsProvider(credentials))
 //                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("oss-cn-hangzhou.aliyuncs.com","oss-cn-hangzhou")).build();
 //
-////        CreateBucketRequest request = new CreateBucketRequest("renyin2", Region.valueOf("oss-cn-hangzhou"));
-////        Bucket bucket = s3Client.createBucket(request);
-//        PutObjectResult putObjectResult = s3Client.putObject("renyin", objectPath, new File(objectPath));
-//        System.out.println(putObjectResult);
+//        S3CommonApi s3CommonApi = new S3(s3Client);
+//        CreateBucketRequest request = new CreateBucketRequest("renyin2", Region.valueOf("oss-cn-hangzhou"));
+//        Bucket bucket = s3CommonApi.createBucket(request);
+//
+
 
         FastDfsStorageClient storageClient = new FastDfsClientBuilder2().build("10.10.10.76:22122","group2");
         String file = storageClient.uploadFile(new File(objectPath));
